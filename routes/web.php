2bb934b51aa0ec2fa698753950c33f19ebb2785a
Sithira Lokuge <sanjithalokuge@gmail.com>
2023-04-25 10:55:29 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/games', 'GamesController@index');
+
+Route::get('/games/create', 'GamesController@create');
+
+Route::get('/games/{game}', 'GamesController@show');
+
+Route::post('/games', 'GamesController@store');
+
+Route::post('/games/{game}/reviews', 'ReviewsController@store');
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
+
+Route::get('/login', 'SessionsController@create');
+Route::post('/login', 'SessionsController@store');
+Route::get('/logout', 'SessionsController@destroy');
+
+
